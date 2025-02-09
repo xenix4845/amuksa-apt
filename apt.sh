e00050191c5eeef7b67cd905ba1cf0145e1f1879
X <156365851+xenix4845@users.noreply.github.com>
2025-02-10 02:47:49 +0900

    HTTPS으로 업데이트

--- apt.sh
+++ apt.sh
@@ -13,8 +13,8 @@ if [ "$os_name" = "Kali" ]; then
     echo "칼리 리눅스용 스크립트를 실행합니다."
     
     cat > /etc/apt/sources.list << EOF
-deb http://mirror.amuksa.com/kali kali-rolling main contrib non-free non-free-firmware
-deb-src http://mirror.amuksa.com/kali kali-rolling main contrib non-free non-free-firmware
+deb https://mirror.amuksa.com/kali kali-rolling main contrib non-free non-free-firmware
+deb-src https://mirror.amuksa.com/kali kali-rolling main contrib non-free non-free-firmware
 EOF
 
     echo "칼리 리눅스 sources.list 파일이 성공적으로 업데이트되었습니다."
@@ -24,10 +24,10 @@ elif [ "$os_name" = "Ubuntu" ]; then
         echo "우분투 23 이하 버전용 스크립트를 실행합니다."
         
         cat > /etc/apt/sources.list << EOF
-deb http://mirror.amuksa.com/ubuntu/ $codename main restricted universe multiverse
-deb http://mirror.amuksa.com/ubuntu/ $codename-updates main restricted universe multiverse
-deb http://mirror.amuksa.com/ubuntu/ $codename-backports main restricted universe multiverse
-deb http://mirror.amuksa.com/ubuntu/ $codename-security main restricted universe multiverse
+deb https://mirror.amuksa.com/ubuntu/ $codename main restricted universe multiverse
+deb https://mirror.amuksa.com/ubuntu/ $codename-updates main restricted universe multiverse
+deb https://mirror.amuksa.com/ubuntu/ $codename-backports main restricted universe multiverse
+deb https://mirror.amuksa.com/ubuntu/ $codename-security main restricted universe multiverse
 EOF
 
         echo "sources.list 파일이 성공적으로 업데이트되었습니다."
@@ -39,13 +39,13 @@ EOF
 
         cat > /etc/apt/sources.list.d/amuksa-ubuntu.sources << EOF
 Types: deb
-URIs: http://mirror.amuksa.com/ubuntu/
+URIs: https://mirror.amuksa.com/ubuntu/
 Suites: $codename $codename-updates $codename-backports
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 
 Types: deb
-URIs: http://mirror.amuksa.com/ubuntu/
+URIs: https://mirror.amuksa.com/ubuntu/
 Suites: $codename-security
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg

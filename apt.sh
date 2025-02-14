bb92112fa0d7a06528798d3e941d3d7ca22a21f5
X <156365851+xenix4845@users.noreply.github.com>
2025-02-15 04:21:59 +0900

    arm64

--- apt.sh
+++ apt.sh
@@ -8,6 +8,7 @@ fi
 os_name=$(lsb_release -is)
 os_version=$(lsb_release -rs)
 codename=$(lsb_release -cs)
+arch=$(dpkg --print-architecture)  # Get the architecture
 
 if [ "$os_name" = "Kali" ]; then
     echo "칼리 리눅스용 스크립트를 실행합니다."
@@ -23,11 +24,17 @@ elif [ "$os_name" = "Ubuntu" ]; then
     if (( $(echo "$os_version < 24" | bc -l) )); then
         echo "우분투 23 이하 버전용 스크립트를 실행합니다."
         
+        if [ "$arch" = "arm64" ]; then
+            base_url="https://mirror.amuksa.com/ubuntu-ports"
+        else
+            base_url="https://mirror.amuksa.com/ubuntu"
+        fi
+        
         cat > /etc/apt/sources.list << EOF
-deb https://mirror.amuksa.com/ubuntu/ $codename main restricted universe multiverse
-deb https://mirror.amuksa.com/ubuntu/ $codename-updates main restricted universe multiverse
-deb https://mirror.amuksa.com/ubuntu/ $codename-backports main restricted universe multiverse
-deb https://mirror.amuksa.com/ubuntu/ $codename-security main restricted universe multiverse
+deb $base_url/ $codename main restricted universe multiverse
+deb $base_url/ $codename-updates main restricted universe multiverse
+deb $base_url/ $codename-backports main restricted universe multiverse
+deb $base_url/ $codename-security main restricted universe multiverse
 EOF
 
         echo "sources.list 파일이 성공적으로 업데이트되었습니다."
@@ -37,15 +44,21 @@ EOF
         
         rm -f /etc/apt/sources.list.d/*.sources
 
+        if [ "$arch" = "arm64" ]; then
+            base_url="https://mirror.amuksa.com/ubuntu-ports"
+        else
+            base_url="https://mirror.amuksa.com/ubuntu"
+        fi
+
         cat > /etc/apt/sources.list.d/amuksa-ubuntu.sources << EOF
 Types: deb
-URIs: https://mirror.amuksa.com/ubuntu/
+URIs: $base_url/
 Suites: $codename $codename-updates $codename-backports
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 
 Types: deb
-URIs: https://mirror.amuksa.com/ubuntu/
+URIs: $base_url/
 Suites: $codename-security
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
